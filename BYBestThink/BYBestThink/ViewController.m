fe2c85d54bf8c601f3ae86ba1919cfb16f6ca991
lby <852811848@qq.com>
2017-04-25 12:45:16 +0800

    创建项目
    创建项目
    1.通过cocoaPods集成常用的第三方.
    2.增加AppIcon和启动图标.

---
+++ BYBestThink/BYBestThink/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BYBestThink
+//
+//  Created by lby on 2017/4/24.
+//  Copyright © 2017年 lby. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ BYBestThink/Pods/Target Support Files/Pods-BYBestThink/Pods-BYBestThink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BYBestThink : NSObject
+@end
+@implementation PodsDummy_Pods_BYBestThink
+@end
